aeaa55f83048721fe26aad9e6e32c04a8325b344
TahirIsmail <vjtary104@gmail.com>
2024-04-05 20:20:51 +0500

    the incubator summary works

---
+++ resources/views/layouts/partials/incubator_summary.blade.php
@@ -0,0 +1,52 @@
+<div class="s_card">
+    <p class="s_card-title">Personal Information</p>
+    <table id="top-table">
+
+        <tr>
+            <td>Name : Farhad</td>
+            <td>Email : farhad@gmail.com</td>
+
+        </tr>
+        <tr>
+            <td>Gender : male</td>
+            <td>Mobile/WhatsApp No : 03370720725</td>
+
+        </tr>
+        <tr>
+            <td>CNIC No : 3740518075655</td>
+            <td>Facebook Profile : 000000</td>
+
+        </tr>
+    </table>
+    <div class="go-corner">
+        <div class="go-arrow">2</div>
+    </div>
+</div>
+
+
+<div class="s_card">
+    <p class="s_card-title">Incubator Payment Summary</p>
+
+
+
+    <table id="top-table">
+        <tr>
+            <th>INCUBATOR CITY</th>
+            <th>SHIFT</th>
+            <th>SUBSCRIPTION PERIOD</th>
+            <th>TOTAL AMOUNT TO DEPOSIT (PKR)</th>
+        </tr>
+        <tr>
+            <td>Lahore</td>
+            <td>evening</td>
+            <td>1 month</td>
+            <td>25,000 PKR</td>
+        </tr>
+
+
+
+    </table>
+    <div class="go-corner">
+        <div class="go-arrow">3</div>
+    </div>
+</div>
