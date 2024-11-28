1759541b562ab4c185dcde99d01d00737cd906f8
Anees Siddique <aneessiddique21@gmail.com>
2024-11-28 14:13:06 +0500

    changes

--- resources/views/digital_incubation/form.blade.php
+++ resources/views/digital_incubation/form.blade.php
@@ -197,7 +197,8 @@
                     if((course1.text() !=  'Select Course' && course2.text() !=  'Select Course') || (course2.text() !=  'Select Course' && course3.text() !=  'Select Course') || (course1.text() !=  'Select Course' && course3.text() !=  'Select Course')){
                         html += `<td>15%</td>`;
                         amount = amount * 0.85;
-                    }else if(course1.text() !=  'Select Course' && course2.text() !=  'Select Course' && course3.text() !=  'Select Course'){
+                    }
+                    if(course1.text() !=  'Select Course' && course2.text() !=  'Select Course' && course3.text() !=  'Select Course'){
                         html += `<td>30%</td>`;
                         amount = amount * 0.70;
                     }else{
